c175d2394da7f5500e406a1865fbdc972fb9c7b3
Chingly <kavisara.noya@mail.kmutt.ac.th>
2021-06-13 18:33:53 +0700

    [FIX] can scroll in Menus

--- lib/widget/layout/menu.dart
+++ lib/widget/layout/menu.dart
@@ -11,43 +11,47 @@ import '../../screen/historyVaccinate_screen.dart';
 class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: kSizeXXL * 1.5,
-      child: Column(
-        children: [
-          Container(
-            alignment: Alignment.topLeft,
-            child: MainText(
-                'Menus', text_type.regular, kFontSizeHeadline4, primary01),
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.topLeft,
+          child: MainText(
+              'Menus', text_type.regular, kFontSizeHeadline4, primary01),
+        ),
+        Container(
+          height: kSizeXXL * 1.19,
+          child: ListView(
+            children: [
+              MenuNavigator(
+                text: 'Appointment',
+                onPressed: () {
+                  Navigator.of(context).pushNamed(Step1.routeName);
+                },
+              ),
+              kSizedBoxXXS,
+              MenuNavigator(
+                text: 'History of Vacation',
+                onPressed: () {
+                  Navigator.of(context)
+                      .pushNamed(HistoryVaccinateScreen.routeName);
+                },
+              ),
+              kSizedBoxXXS,
+              MenuNavigator(
+                text: 'Person',
+                onPressed: () {
+                  Navigator.of(context).pushNamed(PersonScreen.routeName);
+                },
+              ),
+              kSizedBoxXXS,
+              MenuNavigator(
+                text: 'Symptom assessment Form',
+                onPressed: () {},
+              ),
+            ],
           ),
-          kSizedBoxVerticalS,
-          MenuNavigator(
-            text: 'Appointment',
-            onPressed: () {
-              Navigator.of(context).pushNamed(Step1.routeName);
-            },
-          ),
-          kSizedBoxXXS,
-          MenuNavigator(
-            text: 'History of Vacation',
-            onPressed: () {
-              Navigator.of(context).pushNamed(HistoryVaccinateScreen.routeName);
-            },
-          ),
-          kSizedBoxXXS,
-          MenuNavigator(
-            text: 'Person',
-            onPressed: () {
-              Navigator.of(context).pushNamed(PersonScreen.routeName);
-            },
-          ),
-          kSizedBoxXXS,
-          MenuNavigator(
-            text: 'Symptom assessment Form',
-            onPressed: () {},
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
