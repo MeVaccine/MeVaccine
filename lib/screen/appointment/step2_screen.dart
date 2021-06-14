e1c6ff14a0778382cc58a95223bc8d99dc7b9081
Chingly <kavisara.noya@mail.kmutt.ac.th>
2021-06-14 16:31:05 +0700

    [ADD] card vaccine and location form

--- lib/screen/appointment/step2_screen.dart
+++ lib/screen/appointment/step2_screen.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:mevaccine/config/color.dart';
 import 'package:mevaccine/config/constants.dart';
+import 'package:mevaccine/model/authType.dart';
+import 'package:mevaccine/widget/auth/register_TextForm.dart';
 import 'package:mevaccine/widget/button/smallButton.dart';
-import '../../widget/Logo/empty_person.dart';
-import '../../widget/layout/profile_card.dart';
+import 'package:searchable_dropdown/searchable_dropdown.dart';
 import '../../widget/layout/layout_appointment.dart';
 import '../appointment/step3_screen.dart';
 import '../../widget/layout/list_vaccine.dart';
+import 'package:mevaccine/widget/text/mainText.dart';
+import 'package:mevaccine/model/textType.dart';
 
-class Step2 extends StatelessWidget {
+class Step2 extends StatefulWidget {
   static const routeName = '/step2';
+
+  @override
+  _Step2State createState() => _Step2State();
+}
+
+class _Step2State extends State<Step2> {
+  List<String> data = ['one', 'two', 'three', 'four'];
+  String selectedValue = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +34,44 @@ class Step2 extends StatelessWidget {
         ),
         kSizedBoxVerticalS,
         ListVaccine(),
+        kSizedBoxVerticalM,
+        Container(
+            margin: EdgeInsets.only(right: 200),
+            child: MainText(
+                'Location', text_type.regular, kFontSizeHeadline3, primary01)),
+        kSizedBoxVerticalXXS,
+        Container(
+          width: 330,
+          child: RegisterTextForm(
+            label: 'Province',
+            type: RegsiterTextFormType.nothing,
+            active: RegisterActiveType.enable,
+          ),
+        ),
+        //SerachAbleDropDown  ยังไม่ได้ทำ searchFN
+        Container(
+          width: 330,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: kSizeS, vertical: kSizeS),
+            child: SearchableDropdown.single(
+              items: data.map((e) {
+                return DropdownMenuItem<dynamic>(
+                  child: Text(e),
+                );
+              }).toList(),
+              hint: 'Selected',
+              isCaseSensitiveSearch: true,
+              searchHint: const Text('Select your hospital'),
+              onChanged: (value) {
+                setState(() {
+                  selectedValue = value;
+                });
+              },
+              isExpanded: true,
+            ),
+          ),
+        ),
         Container(
           margin: EdgeInsets.symmetric(horizontal: kSizeS),
           child: Align(
@@ -36,7 +85,7 @@ class Step2 extends StatelessWidget {
               width: 120,
             ),
           ),
-        )
+        ),
       ],
     ));
   }
--- lib/widget/layout/list_vaccine.dart
+++ lib/widget/layout/list_vaccine.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mevaccine/config/color.dart';
-import './vaccine.dart';
+import 'package:mevaccine/widget/layout/vaccine.dart';
+import 'package:mevaccine/config/constants.dart';
 
 class ListVaccine extends StatelessWidget {
   const ListVaccine({Key? key}) : super(key: key);
@@ -8,12 +9,30 @@ class ListVaccine extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: primary02,
-      height: 200,
-      width: 315,
-      child: Column(
-        children: [Vaccine()],
-      ),
-    );
+        decoration: BoxDecoration(
+            color: white,
+            borderRadius: kBorderRadiusS,
+            boxShadow: [
+              BoxShadow(
+                  blurRadius: 40,
+                  spreadRadius: 0,
+                  offset: const Offset(0, 16),
+                  color: const Color(0xFF7090B0).withOpacity(0.2))
+            ]),
+        padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+        // color: primary02,
+        height: 170,
+        width: 330,
+        child: GridView.count(
+          childAspectRatio: 4,
+          crossAxisCount: 2,
+          children: [
+            Vaccine(text: 'Sinovac'),
+            Vaccine(text: 'AstraZeneca'),
+            Vaccine(text: 'Moderna'),
+            Vaccine(text: 'Sinopharm'),
+            Vaccine(text: 'Pfizer'),
+          ],
+        ));
   }
 }
