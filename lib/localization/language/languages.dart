7ee92be60094c9ec33710c3a251b2ff81881bb12
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-06-23 14:57:55 +0700

    Arrage

--- lib/localization/language/languages.dart
+++ lib/localization/language/languages.dart
@@ -7,24 +7,23 @@ abstract class Languages {
 
   String get appName;
 
-  // Common Input Label
+  // Common Input Button Label
   String get doneButtonLabel;
   String get okButtonLabel;
-  String get nationalIDInputLabel;
-  String get phoneNumberInputLabel;
   String get loginButtonLabel;
   String get registerButtonLabel;
+  // Common Input Label
+  String get nationalIDInputLabel;
+  String get phoneNumberInputLabel;
 
   // Login Screen
   String get loginHeadingLabel;
   String get invalidNationalIDErrorMessage;
-
   // Verification Code Screen
   String get verificationCodeHeadingLabel;
   String get verificationCodeInputLabel;
   String verificationCodeTextMessage(String phoneNumber, String refCode);
   String get emptyVerificationCodeErrorMessage;
-
   // Register Screen
   String get registerHeadingLabel;
   String get laserIDInputLabel;
