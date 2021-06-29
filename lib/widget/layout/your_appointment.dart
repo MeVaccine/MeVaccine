5a790600689a1006689607d214f39b5d0a4179d7
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-06-29 10:40:41 +0700

    ADD Getting data of appointment from NewAppointmentProvider

--- lib/widget/layout/your_appointment.dart
+++ lib/widget/layout/your_appointment.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:intl/intl.dart';
 import 'package:mevaccine/config/color.dart';
 import 'package:mevaccine/config/constants.dart';
 import 'package:mevaccine/localization/language/languages.dart';
 import 'package:mevaccine/model/textType.dart';
+import 'package:mevaccine/provider/newAppointmentProvider.dart';
+import 'package:provider/provider.dart';
 import '../text/mainText.dart';
 import 'iconWithText.dart';
 
@@ -43,38 +46,46 @@ class YourAppointment extends StatelessWidget {
                       offset: const Offset(0, 16),
                       color: const Color(0xFF7090B0).withOpacity(0.2))
                 ]),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            Languages.of(context)!.yourAppointmentHeading,
-            style: TextStyle(
-                color: checkColor == '1' ? white : primary01,
-                fontSize: 18,
-                fontWeight: FontWeight.w600),
-          ),
-          kSizedBoxS,
-          Row(
+      child: Consumer<NewAppointmentProvider>(
+        builder: (ctx, newAppointmentProvider, _) {
+          DateTime selectedDateTime = newAppointmentProvider
+              .locationDateime[newAppointmentProvider.selectedDateTimeIndex]
+              .startDateTime;
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              IconWithText(
-                color: color,
-                icon: FeatherIcons.calendar,
-                text: '06-03-2021',
+              Text(
+                Languages.of(context)!.yourAppointmentHeading,
+                style: TextStyle(
+                    color: checkColor == '1' ? white : primary01,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600),
               ),
-              kSizedBoxHorizontalM,
-              IconWithText(
-                color: color,
-                icon: FeatherIcons.clock,
-                text: '9:00 AM',
+              kSizedBoxS,
+              Row(
+                children: [
+                  IconWithText(
+                    color: color,
+                    icon: FeatherIcons.calendar,
+                    text: DateFormat.yMd().format(selectedDateTime),
+                  ),
+                  kSizedBoxHorizontalM,
+                  IconWithText(
+                    color: color,
+                    icon: FeatherIcons.clock,
+                    text: DateFormat.Hm().format(selectedDateTime),
+                  ),
+                ],
               ),
+              kSizedBoxVerticalXS,
+              IconWithText(
+                  color: color,
+                  text: Languages.of(ctx)!.locationNameItem(
+                      newAppointmentProvider.selectedLocation),
+                  icon: FeatherIcons.mapPin)
             ],
-          ),
-          kSizedBoxVerticalXS,
-          IconWithText(
-              color: color,
-              text: 'Siriraj Piyamaharajkarun Hospital',
-              icon: FeatherIcons.mapPin)
-        ],
+          );
+        },
       ),
     );
   }
