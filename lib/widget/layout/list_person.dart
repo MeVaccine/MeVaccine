11ed0c790e47171b3c184449787da97b0faab617
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-06-30 17:03:44 +0700

    Conditionaly use FutureBuilder

--- lib/widget/layout/list_person.dart
+++ lib/widget/layout/list_person.dart
@@ -1,45 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:mevaccine/config/color.dart';
-import 'package:mevaccine/config/constants.dart';
-import 'package:mevaccine/localization/language/languages.dart';
-import 'package:mevaccine/provider/newAppointmentProvider.dart';
 import 'package:mevaccine/provider/personProvider.dart';
 import 'package:provider/provider.dart';
 import '../person/card_person_appoint.dart';
 
 class ListPerson extends StatelessWidget {
+  Widget renderListPerson(List<Person> persons) {
+    return ListView(
+      children: persons
+          .map(
+            (person) => CardPersonAppoint(
+              person: person,
+            ),
+          )
+          .toList(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final personProvider = Provider.of<PersonProvider>(context, listen: false);
+    final isPersonLoaded =
+        Provider.of<PersonProvider>(context).person.isNotEmpty;
     return Container(
       height: 300,
       width: 370,
       // color: primary01,
-      child: FutureBuilder(
-        future: personProvider.getPerson(),
-        builder: (ctx, snapshort) =>
-            snapshort.connectionState == ConnectionState.done
-                ? ListView(
-                    children: personProvider.person
-                        .map(
-                          (person) => CardPersonAppoint(
-                            person: person,
-                          ),
-                        )
-                        .toList(),
-                  )
-                : Center(child: CircularProgressIndicator()),
-      ),
+      child: isPersonLoaded
+          ? renderListPerson(personProvider.person)
+          : FutureBuilder(
+              future: personProvider.getPerson(),
+              builder: (ctx, snapshort) =>
+                  snapshort.connectionState == ConnectionState.done
+                      ? renderListPerson(personProvider.person)
+                      : Center(child: CircularProgressIndicator()),
+            ),
     );
   }
 }
-//  list.map((el) =>
-//               Column(
-//                 children: [
-//                   CardPersonAppoint(
-//                     text: ,
-//                   ),
-//                   kSizedBoxVerticalS
-//                 ],
-//               ),
-//             ),
