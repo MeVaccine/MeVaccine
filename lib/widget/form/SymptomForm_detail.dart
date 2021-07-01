5865cef6e6f261aecd73a5868a172b92be9db952
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-01 10:52:03 +0700

    Comment the used file

--- lib/widget/form/SymptomForm_detail.dart
+++ lib/widget/form/SymptomForm_detail.dart
@@ -1,26 +1,26 @@
-import 'package:flutter/material.dart';
-import 'package:mevaccine/config/color.dart';
-import 'package:mevaccine/config/constants.dart';
-import 'package:mevaccine/localization/language/languages.dart';
-import 'package:mevaccine/widget/button/primaryButton.dart';
-import './symptomCard.dart';
+// import 'package:flutter/material.dart';
+// import 'package:mevaccine/config/color.dart';
+// import 'package:mevaccine/config/constants.dart';
+// import 'package:mevaccine/localization/language/languages.dart';
+// import 'package:mevaccine/widget/button/primaryButton.dart';
+// import './symptomCard.dart';
 
-class SymptomFormDetail extends StatefulWidget {
-  @override
-  State<SymptomFormDetail> createState() => _SymptomFormDetailState();
-}
+// class SymptomFormDetail extends StatefulWidget {
+//   @override
+//   State<SymptomFormDetail> createState() => _SymptomFormDetailState();
+// }
 
-class _SymptomFormDetailState extends State<SymptomFormDetail> {
-  @override
-  Widget build(BuildContext context) {
-    bool valueHeadache = false;
-    bool valueNausea = false;
-    bool valueFatigue = false;
-    bool valueChills = false;
-    bool valueMusclePain = false;
-    bool valueTiderness = false;
-    bool valueOther = false;
+// class _SymptomFormDetailState extends State<SymptomFormDetail> {
+//   @override
+//   Widget build(BuildContext context) {
+//     bool valueHeadache = false;
+//     bool valueNausea = false;
+//     bool valueFatigue = false;
+//     bool valueChills = false;
+//     bool valueMusclePain = false;
+//     bool valueTiderness = false;
+//     bool valueOther = false;
 
-    return 
-  }
-}
+//     return 
+//   }
+// }
