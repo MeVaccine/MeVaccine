ddd321e9bdedfb80293dc4cf7d7a35308dbcbfe6
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-03 10:51:17 +0700

    FIX select province to support lang

--- lib/widget/option/hospital_setting.dart
+++ lib/widget/option/hospital_setting.dart
@@ -3,6 +3,7 @@ import 'package:mevaccine/config/color.dart';
 import 'package:mevaccine/config/constants.dart';
 import 'package:mevaccine/localization/language/languages.dart';
 import 'package:mevaccine/provider/authenicateProvider.dart';
+import 'package:mevaccine/provider/newAppointmentProvider.dart';
 import 'package:mevaccine/widget/button/primaryButton.dart';
 import 'package:mevaccine/widget/text/mainText.dart';
 import 'package:provider/provider.dart';
@@ -16,43 +17,45 @@ class HospitalSetting extends StatefulWidget {
 }
 
 class _HospitalSettingState extends State<HospitalSetting> {
-  Future<void> updateHospital() async {
-    String locationID = isProvinceChange
-        ? Provider.of<AuthenicateProvider>(context, listen: false)
-            .hospital[0]
-            .id
-        : updateLocation;
+  // Future<void> updateHospital() async {
+  //   String locationID = isProvinceChange
+  //       ? Provider.of<AuthenicateProvider>(context, listen: false)
+  //           .hospital[0]
+  //           .id
+  //       : updateLocation;
 
-    try {
-      await Provider.of<AuthenicateProvider>(context, listen: false)
-          .updateLocation(noChange
-              ? Provider.of<AuthenicateProvider>(context, listen: false)
-                  .location
-                  .id
-              : locationID);
-      Navigator.of(context)
-          .popUntil(ModalRoute.withName(SettingScreen.routeName));
-    } catch (error) {}
-  }
+  //   try {
+  //     await Provider.of<AuthenicateProvider>(context, listen: false)
+  //         .updateLocation(noChange
+  //             ? Provider.of<AuthenicateProvider>(context, listen: false)
+  //                 .location
+  //                 .id
+  //             : locationID);
+  //     Navigator.of(context)
+  //         .popUntil(ModalRoute.withName(SettingScreen.routeName));
+  //   } catch (error) {}
+  // }
 
-  bool isProvinceChange = false;
-  bool isHospitalChange = false;
-  bool noChange = true;
   String selectedValue = "";
   String selectedHospital = "";
   String updateLocation = '';
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    Provider.of<AuthenicateProvider>(context, listen: false).getLocation();
-    Provider.of<AuthenicateProvider>(context, listen: false)
-        .getHospitalLocation(isProvinceChange
-            ? selectedValue
-            : Provider.of<AuthenicateProvider>(context, listen: false)
-                .location
-                .province_en);
-    return Consumer<AuthenicateProvider>(
-        builder: (context, authen, child) => SafeArea(
+    // Provider.of<AuthenicateProvider>(context, listen: false).getLocation();
+    // Provider.of<AuthenicateProvider>(context, listen: false)
+    //     .getHospitalLocation(isProvinceChange
+    //         ? selectedValue
+    //         : Provider.of<AuthenicateProvider>(context, listen: false)
+    //             .location
+    //             .province_en);
+    return FutureBuilder(
+      future: Provider.of<AuthenicateProvider>(context, listen: false)
+          .getPreferedLocation(),
+      builder: (context, AsyncSnapshot<Location> snapshort) => snapshort
+                  .connectionState ==
+              ConnectionState.done
+          ? SafeArea(
               child: Form(
                 key: _formKey,
                 child: Column(
@@ -61,75 +64,57 @@ class _HospitalSettingState extends State<HospitalSetting> {
                       padding: const EdgeInsets.symmetric(
                           horizontal: kSizeS, vertical: kSizeS),
                       child: SearchableDropdown.single(
-                        items: authen.dataProvince.map((e) {
+                        items: Provider.of<AuthenicateProvider>(
+                          context,
+                          listen: false,
+                        ).dataProvince.map((e) {
                           return DropdownMenuItem<dynamic>(
                             child: Text(
                                 Languages.of(context)!.provinceDropdownItem(e)),
-                            value: e,
+                            value: e['EN'],
                           );
                         }).toList(),
-                        hint: 'Province',
+                        hint: Languages.of(context)!.provinceInputLabel,
                         isCaseSensitiveSearch: true,
-                        searchHint: const Text('Select your province'),
-                        value: isProvinceChange
-                            ? selectedValue
-                            : authen.location.province_en,
+                        searchHint:
+                            Text(Languages.of(context)!.provinceSelectLabel),
+                        value: snapshort.data!.province_en,
                         onChanged: (value) {
-                          setState(() {
-                            isProvinceChange = true;
-                            selectedValue = value;
-                            noChange = false;
-                          });
+                          if (value) {}
                         },
                         isExpanded: true,
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: kSizeS, vertical: kSizeS),
-                      child: SearchableDropdown.single(
-                        items: authen.hospital.map((e) {
-                          return DropdownMenuItem<dynamic>(
-                            child: Text(e.name_en),
-                            value: e.name_en,
-                          );
-                        }).toList(),
-                        hint: 'Hospital',
-                        isCaseSensitiveSearch: true,
-                        searchHint: const Text('Select your hospital'),
-                        value: isProvinceChange
-                            ? isHospitalChange
-                                ? selectedHospital
-                                : authen.hospital[0].name_en
-                            : isHospitalChange
-                                ? selectedHospital
-                                : authen.location.name_en,
-                        onChanged: (value) {
-                          setState(() {
-                            isHospitalChange = true;
-                            selectedHospital = value;
-                            noChange = false;
-                          });
-                          for (int i = 0; i < authen.hospital.length; i++) {
-                            if (selectedHospital ==
-                                authen.hospital[i].name_en) {
-                              setState(() {
-                                updateLocation = authen.hospital[i].id;
-                              });
-                            }
-                          }
-                        },
-                        isExpanded: true,
-                      ),
-                    ),
+                    // Padding(
+                    //   padding: const EdgeInsets.symmetric(
+                    //       horizontal: kSizeS, vertical: kSizeS),
+                    //   child: SearchableDropdown.single(
+                    //     // items: authen.hospital.map((e) {
+                    //     //   return DropdownMenuItem<dynamic>(
+                    //     //     child: Text(e.name_en),
+                    //     //     value: e.name_en,
+                    //     //   );
+                    //     // }).toList(),
+                    //     hint: 'Hospital',
+                    //     isCaseSensitiveSearch: true,
+                    //     searchHint: const Text('Select your hospital'),
+                    //     // value: '',
+                    //     onChanged: (value) {
+                    //       if (value) {}
+                    //     },
+                    //     isExpanded: true,
+                    //   ),
+                    // ),
                     kSizedBoxVerticalS,
                     PrimaryButton(
-                      onPressed: updateHospital,
-                      text: 'Update',
+                      onPressed: () {},
+                      text: Languages.of(context)!.updateButtonLabel,
                     )
                   ],
                 ),
               ),
-            ));
+            )
+          : Center(child: CircularProgressIndicator()),
+    );
   }
 }
