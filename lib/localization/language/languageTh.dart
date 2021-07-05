56be31c076557e901094aba6ace95e376ad1a4c2
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-07-05 19:38:59 +0700

    FIX merge conflict

--- lib/localization/language/languageTh.dart
+++ lib/localization/language/languageTh.dart
@@ -1,8 +1,4 @@
 // ignore_for_file: file_names
-<<<<<<< HEAD
-=======
-
->>>>>>> bb9e316f7ff3b90b529f30c69c7ff61d508ee432
 import 'package:mevaccine/localization/language/languages.dart';
 import 'package:mevaccine/model/httpException.dart';
 import 'package:mevaccine/provider/authenicateProvider.dart';
